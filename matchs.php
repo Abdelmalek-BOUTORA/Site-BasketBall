67da89781d2a0b1e035f6af49aa910409e0fcce8
Abdelmalek-BOUTORA <abdelmalekboutora@gmail.com>
2026-09-05 22:34:13 +0100

    new

--- matchs.php
+++ matchs.php
@@ -52,9 +52,7 @@ $resultat = $conn->query("SELECT * FROM matchs ORDER BY date, heure");
     <meta charset="UTF-8">
     <title>Gestion des matchs</title>
 
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-</head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"></head>
 
 <body>
 
